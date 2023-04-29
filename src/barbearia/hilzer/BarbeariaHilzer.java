3092583bc1d0992e9188e4416f0dc02fca3c2b0a
Gustavo Simon <gustavo.andre.simon@hotmail.com>
2023-04-29 19:27:33 -0300

    Added the exercise description

--- src/barbearia/hilzer/BarbeariaHilzer.java
+++ src/barbearia/hilzer/BarbeariaHilzer.java
@@ -9,6 +9,24 @@ import static barbearia.hilzer.customer.CustomersConstants.*;
 /**
  * Classe principal da solução do problema da barbearia de Hilzer.
  * 
+ * William Stallings apresenta uma versão mais complicada do problema da barbearia, 
+ * que ele atribui a Ralph Hilzer da Universidade da Califórnia. 
+ * 
+ * O problema consiste em uma barbearia com três barbeiros e três cadeiras próprias de barbeiros, 
+ * também existe uma série de lugares para que os clientes possam esperar. 
+ * 
+ * Tanto os barbeiros quanto os clientes devem ser implementados como Threads.
+ * 
+ * [ ] - três cadeiras;
+ * [✓] - três barbeiros; 
+ * [✓] - uma sala de espera com um sofá de quatro lugares;
+ * [ ] - o número total de clientes permitidos na barbearia é 20;
+ * [ ] - nenhum cliente entrará se a capacidade do local estiver satisfeita;
+ * [✓] - se o cliente entrou e tiver lugar no sofá ele se senta, caso contrário ele espera em pé;
+ * [✓] - quando um barbeiro está livre para atender, o cliente que está a mais tempo no sofá é atendido e o que está a mais tempo em pé se senta;
+ * [✓] - qualquer barbeiro pode aceitar pagamento, mas somente um cliente pode pagar por vez, porque só há uma maquina de cartão (POS / TEF);
+ * [✓] - os barbeiros dividem o seu tempo entre cortar cabelo, receber pagamento e dormir enquanto esperam por um cliente.
+ * 
  * @author Gustavo Simon
  */
 public class BarbeariaHilzer {
@@ -171,7 +189,7 @@ public class BarbeariaHilzer {
                         }
                     }
                 } catch (InterruptedException e) {
-                    System.out.println(e.toString());
+                    System.out.println(e);
                 }
             }
 
