6c47424c6b79bbc1c3ff93263900e8b3fda1aa83
Gustavo Simon <gustavo.andre.simon@hotmail.com>
2023-04-28 22:04:21 -0300

    Working on the exercise

--- src/barbearia/hilzer/BarbeariaHilzer.java
+++ src/barbearia/hilzer/BarbeariaHilzer.java
@@ -19,18 +19,18 @@ public class BarbeariaHilzer {
     private final Random random = new Random();
 
     /** Barbeiro número 1 */
-    private final Thread b1 = new Thread(new Barber());
+    private final Thread b1 = new Thread(new Barber("Gilson"));
     /** Barbeiro número 2 */
-    private final Thread b2 = new Thread(new Barber());
+    private final Thread b2 = new Thread(new Barber("Marcos"));
     /** Barbeiro número 3 */
-    private final Thread b3 = new Thread(new Barber());
+    private final Thread b3 = new Thread(new Barber("Barba Negra"));
 
     /** Fila com os clientes que estão esperando no sofá (deve armazenar no máximo 4 pessoas) */
     private final Queue<Customer> couch = new ConcurrentLinkedQueue<Customer>();
-    // /** Fila com os clientes que estão esperando em pé */
-    // private final Queue<Customer> standingsCustomers = new ConcurrentLinkedQueue<Customer>();
-    // /** Fila com o clientes que estão esperando para fazer o pagamento */
-    // private final Queue<Customer> customerPaying = new ConcurrentLinkedQueue<Customer>();
+    /** Fila com os clientes que estão esperando em pé */
+    private final Queue<Customer> standingsCustomers = new ConcurrentLinkedQueue<Customer>();
+    /** Fila com o clientes que estão esperando para fazer o pagamento */
+    private final Queue<Customer> customerPaying = new ConcurrentLinkedQueue<Customer>();
 
     public static void main(String[] args) {
         new BarbeariaHilzer().run();
@@ -40,32 +40,12 @@ public class BarbeariaHilzer {
      * Inicia a execução do problema da barbearia de Hilzer.
      */
     public void run() {
-        // Inicia a thread de geração de clientes que serão atendidos pela barbearia
-        customerGenerator.run();
         // Inicia o trabalho dos barbeiros
-        b1.run();
-        b2.run();
-        b3.run();
-
-    
-    // requisitos:
-    //   4. o número total de clientes permitidos na barbearia é 20;
-    //   5. nenhum cliente entrará se a capacidade do local estiver satisfeita;
-
-    //   6. se o cliente entrou e tiver lugar no sofá ele se senta, caso contrário ele espera em pé;
-    //   7. quando um barbeiro está livre para atender, o cliente que está a mais tempo no sofá é atendido e o que está a mais tempo em pé se senta;
-    //   8. qualquer barbeiro pode aceitar pagamento, mas somente um cliente pode pagar por vez, porque só há uma maquina de cartão (POS / TEF);
-    //   9. os barbeiros dividem o seu tempo entre cortar cabelo, receber pagamento e dormir enquanto esperam por um cliente.       
-    //
-    //  
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-
+        b1.start();
+        b2.start();
+        b3.start();
+        // Inicia a thread de geração de clientes que serão atendidos pela barbearia
+        customerGenerator.start();
     }
 
     /**
@@ -77,10 +57,10 @@ public class BarbeariaHilzer {
         public void run() {
             while (true) {
                 try {
-                    Thread.sleep(random.nextLong(10000));
+                    Thread.sleep(random.nextLong(5000));
                     Customer customer = new Customer(CUSTOMERS_NAMES[random.nextInt(CUSTOMERS_NAMES.length)]);
+                    // System.out.println("Cliente " + customer.getName() + " chegou a barbearia!");
                     customer.run();
-                    System.out.println("Cliente " + customer.getName() + " chegou a barbearia!");
                 } catch (InterruptedException e) {
                     System.out.println(e);
                 }
@@ -104,6 +84,10 @@ public class BarbeariaHilzer {
         /**
          * Retorna o nome do cliente.
          * 
+         * <p>
+         * Obs: Esse atributo não possui método setter 
+         * para garantir a imutabilidade do objeto.
+         * 
          * @return {@code String} nome do cliente
          */
         public String getName() {
@@ -112,20 +96,22 @@ public class BarbeariaHilzer {
 
         @Override
         public void run() {
-            // Sincroniza o acesso a fila do sofá, para que não aconteça buscar um 
+            // Notifica os barbeiros de que um cliente no entrou na barberia
+            // b1.notifyAll();
+            // b2.notifyAll();
+            // b3.notifyAll();
+            // Sincroniza o acesso a fila do sofá 
             synchronized (couch) {
                 // Se o sofá tem espaço livre, o cliente se senta no sofá
                 if (couch.size() < 4) {
+                    System.out.println("O cliente " + this.getName() + " sentou no sofá para aguardar!");
                     couch.add(this);
                     return;
                 };
-
             }
-            // Se o sofá está cheio, verifica se o cliente pode ficar na barbearia
-            // todo: Nesse caso, precisa verificar se a ocupação total da barberia supera 20 clientes
-            // if () {
-
-            // }
+            // Se o sofá está cheio, o cliente fica em pé
+            standingsCustomers.add(this);
+            System.out.println("O cliente " + this.getName() + " está aguardando em pé!");
         }
     }
 
@@ -135,21 +121,61 @@ public class BarbeariaHilzer {
      */
     private class Barber implements Runnable {
 
-        public Barber() {}
+        private final String barberName;
+        
+        public Barber(String barberName) {
+            this.barberName = barberName;
+        }
 
         @Override
         public void run() {
-            try {
-                // Se o sofá está vazio, significa que não há clientes para atender
-                // Nesse caso, o barbeiro dorme
-                if (couch.isEmpty()) {
-                    this.wait();
-                }
-                
-            } catch (InterruptedException e) {
-                // TODO: handle exception
-            }
+            while (true) {
 
+                try {
+                    // Cliente que será atendido
+                    Customer currentCustomer = null;
+                    // Se o sofá está vazio, significa que não há clientes para atender
+                    // Nesse caso, o barbeiro dorme
+                    // synchronized (couch) {
+                    //     if (couch.isEmpty()) {
+                    //         synchronized (this) {
+                    //             this.wait();
+                    //         }
+                    //     }
+                    // Busca o próximo cliente a ser atendido
+                    while (currentCustomer == null) {
+                        currentCustomer = couch.poll();
+                    }
+                    
+                    // Como um cliente que estava sentado no sofá está sendo atendido, 
+                    // o que estava a mais tempo em pé deve se sentar
+                    var nextCustomer = standingsCustomers.poll();
+                    if (nextCustomer != null) {
+                        couch.add(nextCustomer);
+                    }
+                    // }
+    
+                    System.out.println("O barber " + this.barberName + " está atendendo o cliente " + currentCustomer.getName());
+    
+                    // Randomiza o tempo de atendimento pois cada cliente vai demorar um tempo diferente devido 
+                    // ao serviço que vai ser realizado
+                    Thread.sleep(random.nextLong(20000));
+    
+                    // Como o cliente já foi atendido, ele deve ir para a lista de clientes a pagar
+                    customerPaying.add(currentCustomer);
+    
+                    // O barbeiro que foi para a estande de pagamento faz o atendimento a todos os clientes que estão na fila de espera
+                    synchronized (customerPaying) {
+                        while (!customerPaying.isEmpty()) {
+                            var customer = customerPaying.poll();
+                            Thread.sleep(random.nextLong(5000));
+                            System.out.println("O cliente " + customer.getName() + " realizou o pagamento com o barber " + this.barberName + "!");
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    System.out.println(e.toString());
+                }
+            }
 
         }
         
